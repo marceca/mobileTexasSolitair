a5fcb71462e1bc2acf831d2fd40593306156ce38
Mike <mmarceca@aardvarkbrigade.com>
2019-03-14 18:34:15 -0400

    wip

--- App.js
+++ App.js
@@ -3,6 +3,26 @@ import {StyleSheet, View, Dimensions, ScrollView} from 'react-native';
 import { Provider } from 'react-redux';
 import store from './Src/state/store';
 import Game from './Src/components/Game';
+import { Google } from 'expo';
+import signInAsync from './Authentication';
+
+// console.log('google ', Google)
+
+console.log(signInAsync);
+signInAsync()
+// const googleWebAppId = '846083047297-2k0h3gl1orlo511la10m7o570dra5u0j.apps.googleusercontent.com';
+
+// let result = await Expo.AuthSession.startAsync({
+//   authUrl:
+//     `https://accounts.google.com/o/oauth2/v2/auth?` +
+//     `&client_id=${googleWebAppId}` +
+//     `&redirect_uri=${encodeURIComponent('/')}` +
+//     `&response_type=code` +
+//     `&access_type=offline` +
+//     `&scope=profile`,
+// });
+
+// console.log('result ', result)
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
@@ -10,7 +30,7 @@ const screenHeight = Dimensions.get('window').height;
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}><View style={styles.container}><Game /></View></Provider>
+      <Provider store={store}><View style={styles.container}></View></Provider>
     );
   }
 }
